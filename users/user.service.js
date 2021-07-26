8f38394a0bab451f5d95ef4a362f713e1e0b2cce
mohsinalgo <semi.mohsin@gmail.com>
2021-07-26 13:09:43 -0700

    second commit

--- users/user.service.js
+++ users/user.service.js
@@ -5,13 +5,23 @@ const db = require('_helpers/db');
 const User = db.User;
 
 module.exports = {
-    _login,
     _register,
-    getAll,
-    getById,
-    update,
-    delete: _delete
+    _login,
 };
+
+
+async function _register(userData) {
+    if (await User.findOne({ username: userData.username })) {
+        throw 'Username ' + userData.username + 'is already taken';
+    }
+
+    const user = new User(userData);
+    if (userData.password) {
+        user.hash = bcrypt.hashSync(userData.password, 10);
+    }
+    await user.save();
+}
+
 
 async function _login({ username, password }) {
     const user = await User.findOne({ username });
@@ -24,43 +34,3 @@ async function _login({ username, password }) {
     }
 }
 
-async function getAll() {
-    return await User.find();
-}
-
-async function getById(id) {
-    return await User.findById(id);
-}
-
-async function _register(userData) {
-    if (await User.findOne({ username: userData.username })) {
-        throw 'Username '+ userData.username +'is already taken';
-    }
-
-    const user = new User(userData);
-    if (userData.password) {
-        user.hash = bcrypt.hashSync(userData.password, 10);
-    }
-    await user.save();
-}
-
-async function update(id, userData) {
-    const user = await User.findById(id);
-
-    if (!user) throw 'User not found';
-    if (user.username !== userData.username && await User.findOne({ username: userData.username })) {
-        throw 'Username "' + userData.username + '" is already taken';
-    }
-
-    if (userData.password) {
-        userData.hash = bcrypt.hashSync(userData.password, 10);
-    }
-    
-    Object.assign(user, userData);
-
-    await user.save();
-}
-
-async function _delete(id) {
-    await User.findByIdAndRemove(id);
-}
